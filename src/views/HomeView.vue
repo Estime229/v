8f4166171823c639a95e3ad1bec568136b6eb077
Estimé GLITI <116567698+Estime229@users.noreply.github.com>
2023-08-03 17:07:02 -0700

    Votre message de commit ici

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <NavbarComponent class="index" />
+    <DashboardComponent />
+    <FooterComponent />
+  </v-app>
+</template>
+
+<script>
+  import NavbarComponent from '../components/NavbarComponent.vue'
+  import DashboardComponent from '../components/DashboardComponent.vue'
+  import FooterComponent from '../components/FooterComponent.vue'
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      NavbarComponent,
+      DashboardComponent,
+      FooterComponent
+
+    },
+  }
+</script>
+<style scoped>
+.index{
+  z-index: 9999;
+}
+</style>
